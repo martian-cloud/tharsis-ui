dfcb9676e203a18162fe9b046b892ccd4b055bd6
Gregory McDaniel <gregory.mcdaniel@infor.com>
2023-04-21 18:20:58 +0000

    Add Docs link in UI

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@ set -e
 
 # Replace env var placeholder in env.js file
 sed -i 's#__THARSIS_API_ENDPOINT__#'"$THARSIS_API_ENDPOINT"'#g' /bin/www/env.js
+sed -i 's#__THARSIS_DOCUMENTS_URL__#'"$THARSIS_DOCUMENTS_URL"'#g' /bin/www/env.js
 
 exec "$@"
--- public/env.js
+++ public/env.js
@@ -1,4 +1,5 @@
 // eslint-disable-next-line no-undef
 window['env'] = {
-    THARSIS_API_ENDPOINT : '__THARSIS_API_ENDPOINT__',
+    THARSIS_API_ENDPOINT: '__THARSIS_API_ENDPOINT__',
+    THARSIS_DOCUMENTS_URL: '__THARSIS_DOCUMENTS_URL__'
 };
