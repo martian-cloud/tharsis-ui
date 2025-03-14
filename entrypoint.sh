38663f6253fb1fd6a534bdd65720efc8faf01638
Aman Singh <aman.singh@infor.com>
2025-03-14 16:19:54 +0000

    Add support for docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,6 @@ set -e
 sed -i 's#__THARSIS_API_ENDPOINT__#'"$THARSIS_API_ENDPOINT"'#g' /bin/www/env.js
 sed -i 's#__THARSIS_DOCUMENTS_URL__#'"$THARSIS_DOCUMENTS_URL"'#g' /bin/www/env.js
 sed -i 's#__THARSIS_UI_VERSION__#'"$THARSIS_UI_VERSION"'#g' /bin/www/env.js
+sed -i 's#__THARSIS_SUPPORT_URL__#'"$THARSIS_SUPPORT_URL"'#g' /bin/www/env.js
 
 exec "$@"
