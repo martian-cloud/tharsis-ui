686b13978c7fff65318334ea868fbe05d9586d70
Aman Singh <aman.singh@infor.com>
2025-07-18 10:24:13 -0400

    Update markdown and styled code to fix padding issues

--- src/common/StyledCode.tsx
+++ src/common/StyledCode.tsx
@@ -1,15 +1,12 @@
-import { styled, lighten } from '@mui/material';
+import { lighten, styled } from '@mui/material';
 
-interface StyledCodeProps {
-    fontSize?: string;
-}
-
-export const StyledCode = styled('code')<StyledCodeProps>(({ fontSize = '90%', theme }) => ({
-    flexWrap: "wrap",
+export const StyledCode = styled(
+    'code'
+)(({ theme }) => ({
     padding: "2px 4px",
     color: `${theme.palette.text.primary}`,
     backgroundColor: lighten(theme.palette.background.paper, 0.2),
     borderRadius: "4px",
-    fontFamily: "ui-monospace,SFMono-Regular,SF Mono,Menlo,Consolas,Liberation Mono,monospace",
-    fontSize
+    fontSize: "90%",
+    whiteSpace: "nowrap",
 }));
